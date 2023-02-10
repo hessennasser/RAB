86c074bf38c27a0bcf0086de7c08b1c03c1a698b
Hessen Nasser <hessenaloosh@gmail.com>
2023-02-10 19:50:04 +0200

    رأب للتطوير العقاري

---
+++ script.js
@@ -0,0 +1,12 @@
+// start menu in header 
+const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector(".nav");
+menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle("active");
+    nav.classList.toggle("active");
+});
+function closeNave () {
+    menuIcon.classList.toggle("active");
+    nav.classList.toggle("active");
+}
+// end menu in header
